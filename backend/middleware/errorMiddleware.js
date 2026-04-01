f156f927f2e2d13b267c73c4ff970f32b510b16e
Kim <kimskogfelter@outlook.com>
2026-04-01 11:37:17 +0200

    lagt till förklarande kommentar i filen errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,5 @@
 // URL:er eller rutter som inte finns
+// fångar upp URL:er/routrar som inte finns (404)
 export const notFoundEndpoint = (req, res, next) => {
     const error = new Error(`Not found - ${req.originalUrl}`)
     res.status(404)
@@ -6,6 +7,7 @@ export const notFoundEndpoint = (req, res, next) => {
 }
 
 // middleware för error
+// skickar statuskod och felmeddelande till klienten
 export const errorHandler = (error, req, res, next) => {
 
     if(res.headerSent) {
