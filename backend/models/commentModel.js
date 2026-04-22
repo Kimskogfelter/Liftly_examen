e4d6932703a12df1462263d71bc925137ff7bac3
Kim <kimskogfelter@outlook.com>
2026-04-22 13:11:59 +0200

    comment model created

---
+++ backend/models/commentModel.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+// schema for comment
+const commentSchema = new mongoose.Schema({
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    content: {type: String, required: true},
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+   
+}, {timestamps: true})
+
+// post model
+export const Comment = mongoose.model('Comment', commentSchema)
