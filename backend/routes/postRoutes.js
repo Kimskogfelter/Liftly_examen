876415c90d84cb747cfef49f227e95d17f7c019e
Kim <kimskogfelter@outlook.com>
2026-04-22 13:49:23 +0200

    routes for comments created

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createPost, updatePost, likePost, unlikePost, getPost, getPosts, deletePost } from '../controllers/postController.js';
+import { createComment, getComment, getComments, deleteComment } from '../controllers/postController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { upload } from '../middleware/cloudinaryUpload.js'
 
@@ -16,3 +17,11 @@ postRouter.patch('/:id/update', authMiddleware, updatePost) // authMiddleware be
 postRouter.post('/:id/like',authMiddleware, likePost)
 postRouter.delete('/:id/unlike',authMiddleware, unlikePost)
 postRouter.delete('/:id',authMiddleware, deletePost)
+
+
+// ---------------------------- comment routes ---------------------------
+
+postRouter.post('/:postId/comments/create', authMiddleware, upload.single("media"), createComment)
+postRouter.get('/:postId/comments',authMiddleware, getComments)
+postRouter.get('/:postId/comments/:commentId',authMiddleware, getComment)
+postRouter.delete('/:postId/comments/:commentId',authMiddleware, deleteComment)
