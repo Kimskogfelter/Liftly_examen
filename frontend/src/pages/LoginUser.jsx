1abd09bf733e2dac5811b86b638e7201b57dfe35
Kim <kimskogfelter@outlook.com>
2026-05-10 13:22:11 +0200

    login and register user files added to pages folder

---
+++ frontend/src/pages/LoginUser.jsx
@@ -0,0 +1,10 @@
+function LoginUser() {
+  return (
+    <div>
+      <h1>Login User</h1>
+      {/* Login form goes here */}
+    </div>
+  );
+}
+
+export default LoginUser;
---
+++ frontend/src/pages/RegisterUser.jsx
@@ -0,0 +1,10 @@
+function RegisterUser() {
+  return (
+    <div>
+      <h1>Register User</h1>
+      {/* Registration form goes here */}
+    </div>
+  );
+}
+
+export default RegisterUser;
