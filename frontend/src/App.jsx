83d2c532f678602f603c200a8ff80a8ad4304dd3
Kim <kimskogfelter@outlook.com>
2026-05-10 13:17:47 +0200

    routes for home, login, register and feed page added to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // state to check if user is logged in, if not redirect to login page
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <p>Welcome to the App</p>
-        </div>
-      </section>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<h1>Login Page</h1>} />
+          <Route path="/register" element={<h1>Register Page</h1>} />
+          <Route path="/feed" element={userLoggedIn ? <h1>Feed</h1> : <Navigate to="/login" />} />
+        </Routes>
+      </Router>
 
     </>
   )
