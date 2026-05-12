84b89eb009c8abef3afd09b7af9a6a588925a46e
Kim <kimskogfelter@outlook.com>
2026-05-12 10:21:51 +0200

    form added to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/images/liftly-logo.png';
+
 function Login() {
+  
+  // states for form inputs from user
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Login User</h1>
-      {/* Login form goes here */}
-    </div>
+    <section>
+        <div><img src={logo} alt="Liftly logo" />
+        <p>Where training meets community</p>
+        <p>Join us today!</p>
+        </div>
+        <form action="GET">
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input type="submit" value="Log in" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+        </form>
+        <div><p>Don't have an account? <a href="/register">Sign up here</a></p></div>
+    </section>
   );
 }
 
