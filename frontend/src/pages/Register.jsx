0ea5d1bfef6a2f1afbbe8ffeeeafb0d674bdfd04
Kim <kimskogfelter@outlook.com>
2026-05-12 14:10:24 +0200

    show password toggle icon added to register user form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import logo from '../assets/images/liftly-logo.png';
 
 function Register() {
@@ -22,10 +23,25 @@ function Register() {
         <p>Join us today!</p>
         </div>
         <form action="POST">
+          {/* username */}
           <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+          {/* email */}
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+          {/* password */}
+          <div>
+            <input type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="button" onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <FaEye/> : <FaEyeSlash/>}
+            </button>
+          </div>
+          {/* confirm password */}
+          <div>
+            <input type={showPassword ? "text" : "password"} placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+            <button type="button" onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <FaEye/> : <FaEyeSlash/>}
+            </button>
+          </div>
+          {/* Sign up button */}
           <input type="submit" value="Sign up" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
         </form>
         <div><p>Already have an account? <a href="/login">Login here</a></p></div>
