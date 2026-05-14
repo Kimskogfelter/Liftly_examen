5615b4e720bd8f5f7f1d982348f31247ededfca6
Kim <kimskogfelter@outlook.com>
2026-05-14 14:05:32 +0200

    logout componente added to remove current user from local storage when current user logs out

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Logout() {
+
+  const navigate = useNavigate();
+
+  // Use useEffect to clear localStorage and redirect to login page when user logs out
+  React.useEffect(() => {
+    localStorage.removeItem("currentUser");
+    navigate("/login");
+  }, [navigate]);
+
   return (
     <div>
       <h1>Logout User</h1>
