933d3cd28d84183f5e2722af73809b766f066c8a
Kim <kimskogfelter@outlook.com>
2026-05-14 13:57:09 +0200

    profile image component added

--- frontend/src/components/ProfileImage.jsx
+++ frontend/src/components/ProfileImage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-function ProfileImage() {
+function ProfileImage({ image }) {
   return (
-    <div className="w-10 h-10 rounded-full bg-gray-300"></div>
+    <div className="w-10 h-10 rounded-full bg-gray-300">
+      {image && <img src={image} alt="Profile" className="w-full h-full object-cover" />}
+    </div>
   );
 }       
 
