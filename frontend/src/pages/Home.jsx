baae9bbc25bf52cb768d11fec757a0cf060d8a76
Kim <kimskogfelter@outlook.com>
2026-05-15 14:12:01 +0200

    onOpenCreatePost function passed down from Navbar to Home, which sets the showCreatePost state to true when the Create Post button in the Navbar is clicked.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,34 @@
 import { useState, React } from "react";
 import Navbar from "../components/Navbar";
+import CreatePost from "../components/CreatePost";
 
-function Home({currentUser}) {
+function Home({ currentUser }) {
 
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const token = currentUser?.token;
+  const [showCreatePost, setShowCreatePost] = useState(false);
+
+
 
   return (
     <>
       <div className="flex">
-        <Navbar currentUser={currentUser} />
+        {/* Navbar */}
+        {/* onOpenCreatePost prop passed to Navbar */}
+        <Navbar currentUser={currentUser} onOpenCreatePost={() => setShowCreatePost(true)} />
         <section className="flex-1 p-4">
           <h1 className="items-center flex justify-center text-center">Welcome to the Home Page</h1>
-         
+          {/* render CreatePost component when showCreatePost is true */}
+          {
+            showCreatePost && (
+              <CreatePost
+                currentUser={currentUser}
+                onClose={() => setShowCreatePost(false)}
+              />
+            )
+          }
         </section>
       </div>
     </>
