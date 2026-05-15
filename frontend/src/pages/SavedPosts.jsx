b0723b043b51a6a13f5031d3771caa089d388c65
Kim <kimskogfelter@outlook.com>
2026-05-15 11:13:54 +0200

    navbar added to saved posts page

--- frontend/src/pages/SavedPosts.jsx
+++ frontend/src/pages/SavedPosts.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
-function SavedPosts() {
+function SavedPosts({currentUser}) {
   return (
-    <div>
-      <h1>Saved Posts</h1>
-    </div>
+    <>
+      <div className="flex">
+        <Navbar currentUser={currentUser} />
+        <section className="flex-1 p-4">
+          <h1 className="items-center flex justify-center text-center">Welcome to the Saved Posts Page</h1>
+        </section>
+      </div>
+    </>
   );
-}   
+}
 
 export default SavedPosts;
