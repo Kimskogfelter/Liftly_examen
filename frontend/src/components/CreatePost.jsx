a65bd81df6c2dbbc83bc13a6cc3851d4693c4c6d
Kim <kimskogfelter@outlook.com>
2026-05-15 11:55:45 +0200

    create post component and add it to home page

---
+++ frontend/src/components/CreatePost.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function CreatePost({ currentUser }) {
+
+  // Get token, profile photo, and user ID from localStorage through currentUser prop passed down from App.jsx
+  // ? is there to prevent errors if currentUser is null or undefined
+  const token = currentUser?.token;
+  const profileImage = currentUser?.profileImage;
+  const userId = currentUser?.id;
+
+  return (
+    <>
+    <form action="POST" method="post">
+        <textarea name="content" placeholder="What's on your mind?"></textarea>
+        <input type="file" name="media" accept="image/*" />
+        <button type="submit">Post</button>
+    </form>
+    </>
+  );
+}
+
+export default CreatePost;
