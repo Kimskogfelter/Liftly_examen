ac208632e5376976049c12fc81881fd2bd9016cc
Kim <kimskogfelter@outlook.com>
2026-05-17 11:33:43 +0200

    username and profile picture of the user who created the post added to the display post component. Links to the user profile page and single post page added

--- frontend/src/components/DisplayPost.jsx
+++ frontend/src/components/DisplayPost.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function DisplayPost({ post }) {
+
     return (
         <>
-            <div>
-                <h2>{post.content}</h2>
-                {post.media && <img src={post.media} alt="Post media" />}
-            </div>
+            <section>
+                {/* display user information */}
+                <div>
+                    <img src={post.createdBy.profileImage} />
+                    <Link to={`/users/${post.createdBy._id}`}>{post.createdBy.username}</Link> // link to user profile page
+                </div>
+                {/* Display post content */}
+                <Link to={`/posts/${post._id}`}> // link to single post page
+                    <h2>{post.content}</h2>
+                    {post.media && <img src={post.media} alt="Post media" />}
+                </Link>
+            </section>
         </>
     );
 }
