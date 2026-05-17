28029fb54c8af9fe51655b727b00999ee4e754c7
Kim <kimskogfelter@outlook.com>
2026-05-17 13:15:37 +0200

    Profile image component added to display post creator's profile image, and links to user profile and single post page added. Time ago from react-timeago added to display how long ago the post was created.

--- frontend/src/components/DisplayPost.jsx
+++ frontend/src/components/DisplayPost.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfileImage from "./ProfileImage";
+import TimeAgo from "react-timeago";
 
 function DisplayPost({ post }) {
 
@@ -8,13 +10,14 @@ function DisplayPost({ post }) {
             <section>
                 {/* display user information */}
                 <div>
-                    <img src={post.createdBy.profileImage} />
-                    <Link to={`/users/${post.createdBy._id}`}>{post.createdBy.username}</Link> // link to user profile page
+                    <ProfileImage image={post.createdBy.profileImage} />
+                    <Link to={`/users/${post.createdBy._id}`}>{post.createdBy.username}</Link> {/* Link to user profile page */}
                 </div>
                 {/* Display post content */}
-                <Link to={`/posts/${post._id}`}> // link to single post page
+                <Link to={`/posts/${post._id}`}> {/* Link to single post page */}
                     <h2>{post.content}</h2>
                     {post.media && <img src={post.media} alt="Post media" />}
+                    <p><TimeAgo date={post.createdAt} /></p>
                 </Link>
             </section>
         </>
