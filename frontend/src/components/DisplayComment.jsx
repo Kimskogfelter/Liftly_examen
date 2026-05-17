d2fa01d0436acd8606cde169b3c4df9b3901fe5c
Kim <kimskogfelter@outlook.com>
2026-05-17 16:09:11 +0200

    display comment component created to display individual comment data, user information and time since comment was created

---
+++ frontend/src/components/DisplayComment.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ProfileImage from "./ProfileImage";
+import TimeAgo from "react-timeago";
+
+function DisplayComment({ comment }) {
+
+    return (
+        <>
+            <section>
+                {/* display user information */}
+                <div>
+                    <ProfileImage image={comment.createdBy.profileImage} />
+                    <Link to={`/users/${comment.createdBy._id}`}>{comment.createdBy.username}</Link> Link to user profile page
+                </div>
+                {/* Display comment content */}
+                <p>{comment.content}</p>
+                <p><TimeAgo date={comment.createdAt} /></p>
+            </section>
+        </>
+    );
+}
+
+export default DisplayComment;
