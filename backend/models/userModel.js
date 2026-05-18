0be6e7c3b840943964aba753159bb43b91492c18
Kim <kimskogfelter@outlook.com>
2026-05-18 12:12:16 +0200

    default text added to profile bio for user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true, minlength: 5, maxlength: 20},
     email: {type: String, required: true, unique: true, maxlength: 45},
     password: {type: String, required: true, minlength: 10},
-    profileBio: {type: String, default: "", maxlength: 200},
+    profileBio: {type: String, default: "No bio yet.", maxlength: 200},
     profileImage: {type: String, default: "https://res.cloudinary.com/dn3kezspn/image/upload/q_auto/f_auto/v1775115252/Liftly_profile_avatar_image_le0vou.png"},
     followers: [{type: Schema.Types.ObjectId, ref: "User"}],
     following: [{type: Schema.Types.ObjectId, ref: "User"}],
