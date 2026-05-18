70daaaf56d65b1aaa9080b06f6f873239e53a382
Kim <kimskogfelter@outlook.com>
2026-05-19 00:22:42 +0200

    App layout component created to be reused across protected pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,29 +6,33 @@ import Register from './pages/Register';
 import SavedPosts from './pages/SavedPosts';
 import ProfilePage from './pages/ProfilePage';
 import SinglePost from './pages/SinglePost';
-import Logout from './pages/Logout';
-
+import AppLayout from './components/layout/AppLayout';
 
 
 function App() {
 
-const [currentUser, setCurrentUser] = useState(
-  JSON.parse(localStorage.getItem("currentUser")) || null
-);
-const [onlineUsers, setOnlineUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("currentUser")) || null
+  );
+  const [onlineUsers, setOnlineUsers] = useState([]);
 
   return (
     <>
       <Router>
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/login" element={<Login setCurrentUser={setCurrentUser} />} />
-          <Route path="/logout" element={<Logout setCurrentUser={setCurrentUser} />} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/home" element={currentUser ? <Home currentUser={currentUser}/> : <Navigate to="/login" />} />
-          <Route path="/savedPosts" element={currentUser ? <SavedPosts currentUser={currentUser}/> : <Navigate to="/login" />} />
-          <Route path="/users/:userId" element={currentUser ? <ProfilePage currentUser={currentUser}/> : <Navigate to="/login" />} />
-          <Route path="/posts/:postId" element={currentUser ? <SinglePost currentUser={currentUser}/> : <Navigate to="/login" />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected Routes */}
+          <Route element={<AppLayout currentUser={currentUser} setCurrentUser={setCurrentUser} />}>
+            <Route path="/home" element={currentUser ? <Home currentUser={currentUser} /> : <Navigate to="/login" />} />
+            <Route path="/savedPosts" element={currentUser ? <SavedPosts currentUser={currentUser} /> : <Navigate to="/login" />} />
+            <Route path="/users/:userId" element={currentUser ? <ProfilePage currentUser={currentUser} /> : <Navigate to="/login" />} />
+            <Route path="/posts/:postId" element={currentUser ? <SinglePost currentUser={currentUser} /> : <Navigate to="/login" />} />
+          </Route>
+
         </Routes>
       </Router>
 
