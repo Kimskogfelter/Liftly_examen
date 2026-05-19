0c3a108bb6ad43177aa36237aa83fe66dd931966
Kim <kimskogfelter@outlook.com>
2026-05-19 18:14:16 +0200

    tailwind styling added to profile image component

--- frontend/src/components/users/ProfileImage.jsx
+++ frontend/src/components/users/ProfileImage.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 function ProfileImage({ image }) {
   return (
-    <div className="w-10 h-10 rounded-full bg-gray-300">
-      {image && <img src={image} alt="Profile" className="w-full h-full object-cover" />}
+  
+    <div className="w-full h-full rounded-full overflow-hidden">
+      <img 
+        src={image} 
+        alt="Profile" 
+        className="w-full h-full object-cover" 
+      />
     </div>
   );
-}       
+}
 
 export default ProfileImage;
