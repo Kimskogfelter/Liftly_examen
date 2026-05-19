f6849a92ef4e8d5aac9f2a792103ac8fda85e677
Kim <kimskogfelter@outlook.com>
2026-05-19 22:56:38 +0200

    moved handleEditPost and handleDeletePost functions to home.jsx so posts state can update correct

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,45 @@
 import { useState, React } from "react";
 import GetPosts from "../functions/GetPosts";
 import Feed from "../components/Feed";
+import { useParams } from "react-router-dom";
+import SearchBar from "../components/layout/SearchBar";
 
 function Home({ currentUser }) {
 
   const [posts, setPosts] = useState([]);
 
+  // function to handle post editing and update the posts state
+  const handleEditPost = (updatedPost) => {
+    // Map through the posts and update the edited post in the posts state
+    const updatedPosts = posts.map((post) => (post._id === updatedPost._id ? updatedPost : post));
+    setPosts(updatedPosts); // Update the posts state
+  };
+
+  // function to handle post deletion and update the posts state
+  const handleDeletePost = (postId) => {
+    const updatedPosts = posts.filter((post) => post._id !== postId);
+    setPosts(updatedPosts);
+  };
+
   return (
     <>
-        <section className="flex-1 p-4">
-          <h1 className="items-center flex justify-center text-center">Welcome to the Home Page</h1>
-        
-          {/* render GetPosts component to fetch all posts */}
-          <GetPosts
-            currentUser={currentUser}
-            setPosts={setPosts}
-            posts={posts}
-          />
-          {/* render Feed component to display posts */}
-          <Feed
-            posts={posts}
-            onSetPosts={setPosts}
-            currentUser={currentUser}
-          />
-        </section>
-     
+      <SearchBar/>
+      <section className="flex-1 p-4">
+        {/* render GetPosts component to fetch all posts */}
+        <GetPosts
+          currentUser={currentUser}
+          setPosts={setPosts}
+          posts={posts}
+        />
+        {/* render Feed component to display posts */}
+        <Feed
+          posts={posts}
+          currentUser={currentUser}
+          handleEditPost={handleEditPost}
+          handleDeletePost={handleDeletePost}
+        />
+      </section>
+
     </>
   );
 }
