f3ff1fdf1aa3435d00a12aab54f6a3d8f0526368
Kim <kimskogfelter@outlook.com>
2026-05-19 22:50:33 +0200

    passed down the correct props

--- frontend/src/components/posts/PostActionsMenu.jsx
+++ frontend/src/components/posts/PostActionsMenu.jsx
@@ -4,7 +4,7 @@ import { FiEdit2, FiTrash2 } from "react-icons/fi";
 import EditPostModal from "./EditPostModal";
 import DeletePostModal from "./DeletePostModal";
 
-function PostActionsMenu({ post, onEditPost, onDeletePost, currentUser }) {
+function PostActionsMenu({ post, handleDeletePost, handleEditPost, currentUser }) {
 
     const [showEditPostModal, setShowEditPostModal] = useState(false);
     const [showDeletePostModal, setShowDeletePostModal] = useState(false);
@@ -14,8 +14,8 @@ function PostActionsMenu({ post, onEditPost, onDeletePost, currentUser }) {
         <div>
             <button onClick={() => setShowEditPostModal(true)}><FiEdit2 /></button>
             <button onClick={() => setShowDeletePostModal(true)}><FiTrash2 /></button>
-            {showEditPostModal && <EditPostModal currentUser={currentUser} post={post} onEditPost={onEditPost} onClose={() => setShowEditPostModal(false)} />}
-            {showDeletePostModal && <DeletePostModal currentUser={currentUser} post={post} onDeletePost={onDeletePost} onClose={() => setShowDeletePostModal(false)} />}
+            {showEditPostModal && <EditPostModal currentUser={currentUser} post={post} handleEditPost={handleEditPost} onClose={() => setShowEditPostModal(false)} />}
+            {showDeletePostModal && <DeletePostModal currentUser={currentUser} post={post} handleDeletePost={handleDeletePost} onClose={() => setShowDeletePostModal(false)} />}
         </div>
      )  
 
