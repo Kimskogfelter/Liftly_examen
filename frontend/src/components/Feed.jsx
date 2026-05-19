34558e605b2422660ac6fe05ca918e8506646ae1
Kim <kimskogfelter@outlook.com>
2026-05-19 22:44:36 +0200

    removed logic for functions to handle edit and delete posts

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import PostCard from "./posts/PostCard";
 
-function Feed({ posts, currentUser, onSetPosts }) {
-
-    const handleEditPost = (updatedPost) => {
-        // Map through the posts and update the edited post in the posts state
-        const updatedPosts = posts.map((post) => (post._id === updatedPost._id ? updatedPost : post));
-        onSetPosts(updatedPosts); // Update the posts state in the parent component (Home.jsx)
-    };
-
-    const handleDeletePost = (postId) => {
-        // Filter out the deleted post from the posts state
-        const updatedPosts = posts.filter((post) => post._id !== postId);
-        onSetPosts(updatedPosts); // Update the posts state in the parent component (Home.jsx)
-    };
+function Feed({ posts, currentUser, handleDeletePost, handleEditPost  }) {
 
 
     return (
@@ -24,7 +12,7 @@ function Feed({ posts, currentUser, onSetPosts }) {
                     <p>No posts available.</p>
                 ) : (
                     posts.map((post) => (
-                        <PostCard key={post._id} post={post} currentUser={currentUser} onEditPost={handleEditPost} onDeletePost={handleDeletePost} />
+                        <PostCard key={post._id} post={post} currentUser={currentUser} handleEditPost={handleEditPost} handleDeletePost={handleDeletePost} />
                     ))
                 )}
             </div>
