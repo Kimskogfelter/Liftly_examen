66f2f6fed24c3e2e6cc50e45fba72a4c4166e3b8
Kim <kimskogfelter@outlook.com>
2026-05-19 15:51:33 +0200

    handleLikeToggle.js function added to handle like toggling for posts

---
+++ frontend/src/functions/handleLikeToggle.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const handleLikeToggle = async (isLiked, setIsLiked, post, currentUser) => {
+
+    const token = currentUser?.token;
+
+    try {
+
+    if (isLiked) {
+
+        // unlike post
+        const response = await axios.delete(`${import.meta.env.VITE_API_URL}/posts/${post._id}/unlike`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }); (post._id);
+
+        console.log("unlike post data:", response.data);
+        console.log(`Post with id${post._id} have been unliked`)
+
+    } else {
+
+        // like post
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/posts/${post._id}/like`, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }); (post._id);
+
+        console.log("like post data:", response.data);
+        console.log(`Post with id${post._id} have been liked`)
+
+    }
+
+    setIsLiked(prev => !prev); 
+
+        } catch (err) {
+
+        console.error(err);
+
+    }
+
+};
