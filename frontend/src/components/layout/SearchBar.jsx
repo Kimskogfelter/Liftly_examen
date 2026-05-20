7ed0493b7e4cfb865f88099b0c036c9c676e1a2d
Kim <kimskogfelter@outlook.com>
2026-05-20 12:36:14 +0200

    resized searchBar component to be the same size as PostCard component with tailwind

--- frontend/src/components/layout/SearchBar.jsx
+++ frontend/src/components/layout/SearchBar.jsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 
 function SearchBar() {
   return (
-    <form className="flex items-center bg-white/40 backdrop-blur-sm border border-gray-100/70 rounded-xl p-2 w-full max-w-2xl mx-auto mb-2 transition-all focus-within:bg-white/80 focus-within:border-gray-200 focus-within:shadow-sm">
+    <form className="flex items-center bg-white/40 backdrop-blur-sm border border-gray-100/70 rounded-xl p-2 w-full max-w-md mx-auto mb-4 transition-all focus-within:bg-white/80 focus-within:border-gray-200 focus-within:shadow-sm">
       <input
         className="bg-transparent text-xs w-full focus:outline-none placeholder-gray-400 text-gray-700 block px-1"
         type="search"
