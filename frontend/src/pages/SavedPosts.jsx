5ac3a12248715b1e09dd8e599bda84029add64a4
Kim <kimskogfelter@outlook.com>
2026-05-21 18:46:08 +0200

    axios get request and Post feed component added to saved posts page

--- frontend/src/pages/SavedPosts.jsx
+++ frontend/src/pages/SavedPosts.jsx
@@ -1,12 +1,55 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Navbar from "../components/layout/Navbar";
+import PostFeed from "../components/posts/PostFeed";
 
-function SavedPosts({currentUser}) {
+function SavedPosts({ currentUser }) {
+
+  const [error, setError] = useState("");
+  const token = currentUser?.token;
+  const [posts, setPosts] = useState([]);
+
+  // function to fetch saved posts
+  const getSavedPosts = async () => {
+    try {
+      // fetched saved posts from backend
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/users/${currentUser.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log("Saved posts fetched successfully:", response.data.user);
+
+      // update the saved posts state with the fetched user information
+      setSavedPosts(response.data.user.savedPosts);
+
+    } catch (err) {
+      // handle errors and display error message to user
+      const errorResponse = err.response?.data;
+      setError(errorResponse?.message || "Saved posts could not be fetched. Please try again.");
+    }
+  };
+
+  // call getSavedPosts function
+  useEffect(() => {
+    getSavedPosts();
+  }, []);
+
   return (
     <>
       <div className="flex">
         <section className="flex-1 p-4">
-          <h1 className="items-center flex justify-center text-center">Welcome to the Saved Posts Page</h1>
+          <h1 className="items-center flex justify-center text-center">Your saved posts</h1>
+          {/* display user's saved posts */}
+          <div>
+            {/* render Post feed component to display posts */}
+            <PostFeed
+              posts={posts}
+              currentUser={currentUser}
+            />
+          </div>
         </section>
       </div>
     </>
