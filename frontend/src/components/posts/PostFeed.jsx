46bbb501d1f3d7cd790d884ca80f79f7e4c1cd36
Kim <kimskogfelter@outlook.com>
2026-05-24 23:01:06 +0200

    prop layout added to take information from pages about layout for grid or list. variable feedLayout created to add styling that was sent down with prop to the parent container of the PostFeed component

--- frontend/src/components/posts/PostFeed.jsx
+++ frontend/src/components/posts/PostFeed.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PostCard from "./PostCard";
 
-function PostFeed({ posts, currentUser, handleDeletePost, handleEditPost  }) {
+function PostFeed({ posts, currentUser, handleDeletePost, handleEditPost, layout }) {
+
+
+    // 
+    const feedLayout = layout === "grid-3x3" 
+    ? "grid grid-cols-3 gap-2 md:gap-4" // 3x3 layout
+    : "flex flex-col gap-4 w-full max-w-2xl mx-auto"; // Long list at home page
 
 
     return (
         <>
-            <div>
+            <div className={feedLayout}>
                 {/* if no posts available */}
                 {posts.length === 0 ? (
                     <p className="text-center">No posts available.</p>
