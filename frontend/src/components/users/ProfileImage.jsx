3cc99f9a37ab9e63a52affcaad5c1d31d8aacb91
Kim <kimskogfelter@outlook.com>
2026-05-25 10:38:25 +0200

    currentUser prop added insted for better understanding in code.. i think.. and added default import of profile image in cause a user is not logged in, or a visitor is viewing the home page

--- frontend/src/components/users/ProfileImage.jsx
+++ frontend/src/components/users/ProfileImage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import defaultProfileImage from "../../assets/images/Liftly_profile_avatar_image.png";
 
-function ProfileImage({ image }) {
+function ProfileImage({ currentUser }) {
+
   return (
-  
+
     <div className="w-full h-full rounded-full overflow-hidden">
-      <img 
-        src={image} 
-        alt="Profile" 
-        className="w-full h-full object-cover" 
+      <img
+        src={currentUser?.profileImage || defaultProfileImage} // if logged in user show logged in users profile image, else show default image imported at the top
+        alt="Profile"
+        className="w-full h-full object-cover"
       />
     </div>
   );
