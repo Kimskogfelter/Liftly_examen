c3ef2b99e006781e034e2bb8329269673fa95e49
Kim <kimskogfelter@outlook.com>
2026-05-25 10:01:33 +0200

    removed home from protected routing so it can be viewed by visitors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
           {/* Protected Routes */}
           <Route element={<AppLayout currentUser={currentUser} setCurrentUser={setCurrentUser} />}>
-            <Route path="/home" element={currentUser ? <Home currentUser={currentUser} /> : <Navigate to="/login" />} />
+            <Route path="/home" element={<Home currentUser={currentUser} />} /> {/* NOT protected, uses the AppLayout component to render layout and navbar */}
             <Route path="/savedPosts" element={currentUser ? <SavedPosts currentUser={currentUser} /> : <Navigate to="/login" />} />
             <Route path="/users/:userId" element={currentUser ? <ProfilePage currentUser={currentUser} /> : <Navigate to="/login" />} />
             <Route path="/posts/:postId" element={currentUser ? <SinglePost currentUser={currentUser} /> : <Navigate to="/login" />} />
