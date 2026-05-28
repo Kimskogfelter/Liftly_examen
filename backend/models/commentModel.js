b21d06690fc5e86db453d3da0328b64ddafd4536
Kim <kimskogfelter@outlook.com>
2026-05-28 14:35:39 +0200

    likes field added to comment model

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -5,6 +5,7 @@ const commentSchema = new mongoose.Schema({
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
     content: {type: String, required: true},
     post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
    
 }, {timestamps: true})
 
