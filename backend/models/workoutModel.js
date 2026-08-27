988e363c086192c1689d91e87dcf4d66e1845415
Kim <kimskogfelter@outlook.com>
2026-08-27 12:44:18 +0200

    title added to workout model to be able to display the title of the workout in the frontend.

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -1,18 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-// Schema for each exercise
 const exerciseSchema = new Schema({
     name: { type: String, required: true },
     sets: { type: Number, required: true, default: 3 },
     reps: { type: Number, required: true, default: 10 },
-    kgs: { type: Number, required: true, default: 10 }
+    kgs: { type: Number, required: true, default: 0 } // Ändrade default till 0 kg
 });
 
-// Schema for workout session
 const workoutSchema = new Schema({
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    day: { type: String, required: true }, 
-    exercises: [exerciseSchema] 
+    day: { 
+        type: String, 
+        required: true,
+        enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] // Låser till giltiga dagar
+    },
+    title: { type: String, required: true }, // ex: "Legs & Glutes", "Push Day", "Rest & Recovery"
+    exercises: [exerciseSchema]
 }, { timestamps: true });
 
 export const Workout = mongoose.model('Workout', workoutSchema);
