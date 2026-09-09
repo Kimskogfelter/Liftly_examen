b6a218896727932efba806480a0ecbffc7cbf78e
Kim <kimskogfelter@outlook.com>
2026-09-09 10:43:26 +0200

    added two new fields to the user model: resetPasswordToken and resetPasswordExpire to handle password reset functionality.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,17 +2,19 @@ import mongoose, { Schema } from "mongoose";
 
 // schema for user
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, minlength: 5, maxlength: 20},
-    email: {type: String, required: true, unique: true, maxlength: 45},
-    password: {type: String, required: true, minlength: 10},
-    profileBio: {type: String, default: "No bio yet.", maxlength: 200},
-    profileImage: {type: String, default: "https://res.cloudinary.com/dn3kezspn/image/upload/q_auto/f_auto/v1775115252/Liftly_profile_avatar_image_le0vou.png"},
-    followers: [{type: Schema.Types.ObjectId, ref: "User"}],
-    following: [{type: Schema.Types.ObjectId, ref: "User"}],
-    savedPosts: [{type: Schema.Types.ObjectId, ref: "Post"}],
-    posts: [{type: Schema.Types.ObjectId, ref: "Post"}],
+    username: { type: String, required: true, minlength: 5, maxlength: 20 },
+    email: { type: String, required: true, unique: true, maxlength: 45 },
+    password: { type: String, required: true, minlength: 10 },
+    profileBio: { type: String, default: "No bio yet.", maxlength: 200 },
+    profileImage: { type: String, default: "https://res.cloudinary.com/dn3kezspn/image/upload/q_auto/f_auto/v1775115252/Liftly_profile_avatar_image_le0vou.png" },
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    savedPosts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    resetPasswordToken: { type: String, default: null },
+    resetPasswordExpires: { type: Date, default: null },
 
-}, {timestamps: true})
+}, { timestamps: true })
 
 // user model
 export const User = mongoose.model('User', userSchema)
