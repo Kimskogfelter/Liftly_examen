fbcad8ccc3ed223290edc5fcc6bf159d7ed48782
Kim <kimskogfelter@outlook.com>
2026-09-17 12:12:49 +0200

    added and imported route to logoutUser controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { registerUser, loginUser, getUser, getUsers, updateUser, followUser, unfollowUser, changeProfileImage, deleteUser, getSavedPosts, authUser, forgotPassword, resetPassword, refreshToken} from '../controllers/userController.js';
+import { registerUser, loginUser, logoutUser, getUser, getUsers, updateUser, followUser, unfollowUser, changeProfileImage, deleteUser, getSavedPosts, authUser, forgotPassword, resetPassword, refreshToken} from '../controllers/userController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { upload } from '../middleware/cloudinaryUpload.js'
 
@@ -11,6 +11,7 @@ export const userRouter = Router();
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 userRouter.post('/refresh', refreshToken);
+userRouter.post('/logout', logoutUser);
 
 userRouter.get('/', authMiddleware, getUsers)
 
