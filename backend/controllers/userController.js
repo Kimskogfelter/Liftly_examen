9b8e72ed1c002f99a196db4102f373cca3b39c50
Kim <kimskogfelter@outlook.com>
2026-09-24 15:30:08 +0200

    feat(user): update updateUser controller to handle socialLinks

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -298,13 +298,13 @@ export const updateUser = async (req, res, next) => {
     try {
 
         // get username, email and/or profile bio from frontend
-        const { username, email, profileBio } = req.body;
+        const { username, email, profileBio, socialLinks } = req.body;
 
         // update only the fields sent in req.body
         // check that the correct user are doing the changes via req.user.id and auth middleware
         const updatedUser = await User.findByIdAndUpdate(
             req.user.id,
-            { $set: { username, email, profileBio } }, // $set ONLY updates the data that is sent from frontend, ex if only profile bio is edited only that is changed in the database
+            { $set: { username, email, profileBio, socialLinks } }, // $set ONLY updates the data that is sent from frontend, ex if only profile bio is edited only that is changed in the database
             { new: true, runValidators: true } // runValidators make sure that the backend rules (such as unique email, minLenght etc that is set in the user model) still follows 
         ).select("-password"); // removes the hashed password so it doenst get sent to frontend
 
